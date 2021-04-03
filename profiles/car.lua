f01b3cc872ae3c8dd6c9b4a72174d796b37e940f
answerquest <nikhil.js@gmail.com>
2021-04-03 08:02:38 +0530

    lowered speed limits

--- profiles/car.lua
+++ profiles/car.lua
@@ -342,18 +342,7 @@ function process_node(profile, node, result, relations)
       local bollard = node:get_value_by_key("bollard")
       local rising_bollard = bollard and "rising" == bollard
 
-      -- make an exception for lowered/flat barrier=kerb
-      -- and incorrect tagging of highway crossing kerb as highway barrier
-      local kerb = node:get_value_by_key("kerb")
-      local highway = node:get_value_by_key("highway")
-      local flat_kerb = kerb and ("lowered" == kerb or "flush" == kerb)
-      local highway_crossing_kerb = barrier == "kerb" and highway and highway == "crossing"
-
-      if not profile.barrier_whitelist[barrier]
-                and not rising_bollard
-                and not flat_kerb
-                and not highway_crossing_kerb
-                or restricted_by_height then
+      if not profile.barrier_whitelist[barrier] and not rising_bollard or restricted_by_height then
         result.barrier = true
       end
     end
